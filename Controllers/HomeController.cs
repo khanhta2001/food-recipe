3782bac286a7aafb3550a241cb36a747edae4306
khanhta2001 <ktran2@iwu.edu>
2023-02-02 23:40:19 -0600

    added basic public functions

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,30 +22,140 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    public IActionResult RegisterPage()
+    {
+        return View();
+    }
+    
+    public IActionResult Register(string username, string email, string password, string retypePassword)
+    {
+        return LoginPage();
+    }
 
     public IActionResult LoginPage()
     {
         return View();
     }
-
-    public IActionResult RegisterPage()
+    
+    public IActionResult Login(string username, string email, string password, string retypePassword)
     {
-        return View();
+        return HomePage();
+    }
+    
+    public IActionResult VerificationPage(string username, string email, string password, string retypePassword)
+    {
+        return HomePage();
+    }
+    
+    public IActionResult ResetPasswordPage()
+    {
+        return HomePage();
+    }
+    
+    public IActionResult ResetPassword()
+    {
+        return HomePage();
+    }
+    
+    public IActionResult Verification(string username, string email, string password, string retypePassword)
+    {
+        return HomePage();
     }
 
     public IActionResult Categories()
     {
         return View();
     }
+    
+    public IActionResult Categories_recipePage()
+    {
+        return View();
+    }
+    
+    public IActionResult NotificationPage()
+    {
+        return View();
+    }
+    
+    public IActionResult Notification()
+    {
+        return View();
+    }
+    
+    public IActionResult save_recipe()
+    {
+        return View();
+    }
+    
+    public IActionResult ReviewPage()
+    {
+        return View();
+    }
 
-    public IActionResult Login(string username, string email, string password, string retypePassword)
+    public IActionResult UserAccount()
+    {
+        return View();
+    }
+
+    public IActionResult review_recipePage()
+    {
+        return View();
+    }
+
+    public IActionResult ReviewEditPage()
+    {
+        return View();
+    }
+    
+    public IActionResult ReviewEdit()
+    {
+        return View();
+    }
+
+    public IActionResult SearchPage()
+    {
+        return View();
+    }
+    
+    public IActionResult Search()
+    {
+        return View();
+    }
+    
+    public IActionResult UpdateProfilePage()
+    {
+        return View();
+    }
+    
+    public IActionResult UpdateProfile()
+    {
+        return View();
+    }
+    
+    public IActionResult CreateRecipePage()
     {
         return HomePage();
     }
-
-    public IActionResult Register(string username, string email, string password, string retypePassword)
+    
+    public IActionResult CreateRecipe()
     {
-        return LoginPage();
+        return HomePage();
+    }
+    
+    public IActionResult EditRecipePage()
+    {
+        return HomePage();
+    }
+    
+    public IActionResult EditRecipe()
+    {
+        return HomePage();
+    }
+    
+    public IActionResult ViewRecipe()
+    {
+        return HomePage();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
