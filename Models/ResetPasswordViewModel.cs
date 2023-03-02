b42fd8af3fd9fe660198b96b1a2098a0e2e24c9d
khanhta2001 <ktran2@iwu.edu>
2023-03-02 01:50:20 -0600

    added some models

---
+++ Models/ResetPasswordViewModel.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebApp.Models
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        public string? Username { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Old Password")]
+        public string? OldPassword { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(255, ErrorMessage = "Password must be between 5 and 255 characters", MinimumLength = 5)]
+        [Display(Name = "New Password")]
+        public string? NewPassword { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        [Compare("NewPassword")]
+        public string? NewPasswordConfirm { get; set; }
+    }   
+}
