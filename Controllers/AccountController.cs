5469679d483d93fbd65a1082f8452002eade6aa4
khanhta2001 <ktran2@iwu.edu>
2023-03-01 18:13:50 -0600

    files versioning, controllers -> correct views

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("UpdateProfile")]
+        public IActionResult UpdateProfile()
+        {
+            return View("UpdateProfile");
+        }
+        
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("UserAccount")]
+        public IActionResult UserAccount()
+        {
+            return View("UpdateProfile");
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,172 +3,44 @@ using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
 using Microsoft.AspNetCore.Http;
 
-namespace WebApp.Controllers;
-    
-public class HomeController : Controller
+namespace WebApp.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult HomePage()
-    {
-        HttpContext.Session.SetString("username", "Khanh");
-        //HttpContext.Session.SetString("SessionKeyName", "The Doctor");
-        return View();
-    }
+        public IActionResult HomePage()
+        {
+            HttpContext.Session.SetString("username", "Khanh");
+            //HttpContext.Session.SetString("SessionKeyName", "The Doctor");
+            return View();
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-    
-    public IActionResult RegisterPage()
-    {
-        return View();
-    }
-    
-    public IActionResult Register(string username, string email, string password, string retypePassword)
-    {
-        return LoginPage();
-    }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-    public IActionResult LoginPage()
-    {
-        return View();
-    }
-    
-    public IActionResult Login(string username, string email, string password, string retypePassword)
-    {
-        return HomePage();
-    }
-    
-    public IActionResult Logout()
-    {
-        return HomePage();
-    }
+        public IActionResult NotificationsPage()
+        {
+            return View();
+        }
+        
+        public IActionResult Notification()
+        {
+            return HomePage();
+        }
 
-    public IActionResult ResetPasswordPage()
-    {
-        return HomePage();
-    }
-    
-    public IActionResult ResetPassword()
-    {
-        return HomePage();
-    }
-    
-    public IActionResult VerificationPage(string username, string email, string password, string retypePassword)
-    {
-        return View();
-    }
-    
-    public IActionResult Verification(string username, string email, string password, string retypePassword)
-    {
-        return HomePage();
-    }
-
-    public IActionResult Categories()
-    {
-        return View();
-    }
-    
-    public IActionResult Categories_recipes()
-    {
-        return View();
-    }
-    
-    public IActionResult NotificationsPage()
-    {
-        return View();
-    }
-    
-    public IActionResult Notification()
-    {
-        return HomePage();
-    }
-    
-    public IActionResult save_recipesPage()
-    {
-        return View();
-    }
-    
-    public IActionResult save_recipes()
-    {
-        return HomePage();
-    }
-    
-    public IActionResult ReviewPage()
-    {
-        return View();
-    }
-
-    public IActionResult UserAccount()
-    {
-        return View();
-    }
-
-    public IActionResult ReviewEditPage()
-    {
-        return View();
-    }
-    
-    public IActionResult ReviewEdit()
-    {
-        return HomePage();
-    }
-
-    public IActionResult SearchResults()
-    {
-        return View();
-    }
-    
-    public IActionResult Search()
-    {
-        return HomePage();
-    }
-    
-    public IActionResult UpdateProfilePage()
-    {
-        return View();
-    }
-    
-    public IActionResult UpdateProfile()
-    {
-        return HomePage();
-    }
-    
-    public IActionResult CreateRecipePage()
-    {
-        return View();
-    }
-    
-    public IActionResult CreateRecipe()
-    {
-        return HomePage();
-    }
-    
-    public IActionResult EditRecipePage()
-    {
-        return View();
-    }
-    
-    public IActionResult EditRecipe()
-    {
-        return HomePage();
-    }
-    
-    public IActionResult ViewRecipePage()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
     }
 }
+    
