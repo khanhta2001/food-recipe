a3844f822032502637dbf66ce77c8c620c57c493
khanhta2001 <ktran2@iwu.edu>
2023-03-05 20:07:25 -0600

    added ReviewViewModel.cs

---
+++ Models/ReviewViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebApp.Models
+{
+    public class ReviewViewModel
+    {
+        [Required]
+        public string? Review { get; set; }
+    }   
+}
