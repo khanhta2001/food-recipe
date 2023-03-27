2fb57ab4a635c49e93d952c16864fa9a74f51fde
khanhta2001 <ktran2@iwu.edu>
2023-03-27 00:12:28 -0500

    added files for searchResults

--- Services/DataService.cs
+++ Services/DataService.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Http;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Net;
 using System.Net.Mail;
-using WebApp.Models;
-using Microsoft.Extensions.Options;
 using MongoDB.Driver;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
 using MongoDB.Bson;
+using WebApp.Models;
 
 namespace WebApp.Services
 {
@@ -18,6 +21,31 @@ namespace WebApp.Services
         public DataService(IMongoDatabase mongoDatabase)
         {
             _mongoDatabase = mongoDatabase;
+        }
+
+        public List<RecipeViewModel> FindAllRecipes(string variableName, string collectionName)
+        {
+            var collection = _mongoDatabase.GetCollection<RecipeViewModel>(collectionName);
+            
+            var filter = Builders<RecipeViewModel>.Filter.Or(
+                Builders<RecipeViewModel>.Filter.Eq("Title", new BsonRegularExpression(".*{variableName}.*", "i")), 
+                          Builders<RecipeViewModel>.Filter.Eq("Category", new BsonRegularExpression(".*{variableName}.*", "i")),
+                          Builders<RecipeViewModel>.Filter.Eq("Content", new BsonRegularExpression(".*{variableName}.*", "i")));
+            
+            var allRecipe = collection.Find(filter);
+            
+            return allRecipe.ToList();
+        }
+        
+        public T? FindVariable<T>(string variableName, string collectionName)
+        {
+            var recipeCollection = _mongoDatabase.GetCollection<T>(collectionName);
+            
+            var filter = Builders<T>.Filter.Eq("Title", new BsonRegularExpression(".*{variableName}.*", "i"));
+            
+            var allRecipe = recipeCollection.Find(filter).FirstOrDefault();
+            
+            return allRecipe;
         }
     }   
 }
