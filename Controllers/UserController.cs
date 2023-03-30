385f20d1ccc3e69fe50829081fcb8798b71c84f5
An Khanh Tran <ktran2@iwu.edu>
2023-03-29 23:31:50 -0500

    deleted using methods

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,12 +1,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Mail;
-using Microsoft.AspNetCore.Identity;
 using WebApp.Models;
 using WebApp.Services;
 
