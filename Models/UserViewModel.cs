d8e0ce83113c06d68faa19b5c7c27442d962f1d4
An Khanh Tran <ktran2@iwu.edu>
2023-04-01 02:23:33 -0500

    fixed problems with mongoDB when called

--- Models/UserViewModel.cs
+++ Models/UserViewModel.cs
@@ -1,11 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 
 namespace FoodRecipe.Models
 {
     public class UserViewModel
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
         public string? Username { get; set; }
+
+        public string? Email { get; set; }
 
         public string? Password { get; set; }
 
