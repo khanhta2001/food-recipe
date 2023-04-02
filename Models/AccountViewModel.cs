521afe1dcfb87f441f3b2f5c42790e2dd199fd53
An Khanh Tran <ktran2@iwu.edu>
2023-04-02 01:13:10 -0500

    added resetpassword

--- Models/AccountViewModel.cs
+++ Models/AccountViewModel.cs
@@ -1,7 +1,14 @@
-﻿namespace FoodRecipe.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FoodRecipe.Models
 {
     public class AccountViewModel
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        
         public string? Username { get; set; }
 
         public string? Summary { get; set; } = "Add it here!";
