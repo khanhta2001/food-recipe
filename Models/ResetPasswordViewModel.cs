205b0a91dcdbedf72d225d17a4b76fc72d364a54
An Khanh Tran <ktran2@iwu.edu>
2023-04-04 20:30:53 -0500

    fixed and added resetPassword

--- Models/ResetPasswordViewModel.cs
+++ Models/ResetPasswordViewModel.cs
@@ -5,6 +5,7 @@ namespace FoodRecipe.Models
 {
     public class ResetPasswordViewModel
     {
+        
         [Required]
         [DataType(DataType.Password)]
         [StringLength(255, ErrorMessage = "Password must be between 5 and 255 characters", MinimumLength = 5)]
